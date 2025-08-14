8605391ae715ca1e13d1b208050c5fe46406004f
Shivam Tiwari <shivofficial04@gmail.com>
2025-08-14 16:49:31 +0530

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -76,7 +76,7 @@ export default function Login() {
                 <div className="full-line"></div>
                 <footer>
                     <a target="_blank" href="https://github.com/SyntaxTiwari11/nextjs-15-course">
-                        <img alt="pfp" src="https://avatars.githubusercontent.com/u/77163783?v=4" />
+                        <img alt="pfp" src="https://avatars.githubusercontent.com/u/223843301?v=4" />
                         <h6>@jamezmca</h6>
                         <i className="fa-brands fa-github"></i>
                     </a>
